c4cfab3005c3c393ea9272ef1ecd7ab4f06df47c
Josh Bebbington <josh@focalwebsites.com>
2015-03-16 23:01:33 +0000

    4 of 7 passing

--- Answer.js
+++ Answer.js
@@ -1,26 +1,43 @@
 var $ = function (selector) {
   var elements = [];
+  var elementWithId, elementsWithTag, elementsWithClass = [];
+  var arr = [];
+  var selectors = selector.split(/(?=#)|(?=\.)/);
 
-  var all = document.body.getElementsByTagName('*');
+  var addAllElementsFrom = function(array){
+    for (var i=0; i < array.length; i++){
+      elements.push(array[i]);
+    }
+  }
 
-  for(var i=0; i <= all.length; i++) {
-    if (matchesSelector(selector, all[i]) === true) {
-      elements.push(all[i])
+  var addElementsWithIdentical = function(id, tag){
+    id === tag ? elements.push(id) : elements = []
+  }
+
+  for(var i=0; i < selectors.length; i++){
+    if (selectors[i].indexOf('#') > -1){
+      idSel = selectors[i].slice(1);
+      elementWithId = document.getElementById(idSel);
+    } else if (selectors[i].indexOf('.') > -1){
+      classSel = selectors[i].slice(1);
+      elementsByClass = document.getElementsByClassName(classSel)
+      elementsWithClass = [].slice.call(elementsByClass)
+    } else {
+      tagSel = selectors[i];
+      elementsByTag = document.getElementsByTagName(tagSel);
+      elementsWithTag = [].slice.call(elementsByTag);
+    }
+  }
+
+  if (elementWithId){
+    if (elementsWithTag){
+      for (var i=0; i < elementsWithTag.length; i++){
+        addElementsWithIdentical(elementWithId, elementsWithTag[i]);
+      }
+    } else {
+      elements.push(elementWithId);
     }
   }
 
   return elements;
-}
-
-var matchesSelector = function(selector, element) {
-
-  var expected = 88888;
-
-  element.addRule(selector, 'z-index:' + expected + ';', -1);
-
-  var match = element.currentStyle.zIndex == expected;
-
-  element.removeRule(rules.length-1);
-
-  return match;
 }
