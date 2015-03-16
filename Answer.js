3309888b46dff6b6eb00bc2af331e8d52982dc3e
Josh Bebbington <josh@focalwebsites.com>
2015-03-16 23:17:25 +0000

    Further reduces complexity of conditional functions

--- Answer.js
+++ Answer.js
@@ -16,10 +16,14 @@ var $ = function (selector) {
 
   var addSameElements = function(tags, classes){
     for (var i=0; i < tags.length; i++){
-      for (var n=0; n < classes.length; n++){
-        if (tags[i] === classes[n]){
-          elements.push(tags[i]);
-        }
+      addClassElementsFor(tags, classes, i)
+    }
+  }
+
+  var addClassElementsFor = function(tags, classes, i) {
+    for (var n=0; n < classes.length; n++){
+      if (tags[i] === classes[n]){
+        elements.push(tags[i]);
       }
     }
   }
@@ -34,6 +38,24 @@ var $ = function (selector) {
 
   var getDomElement = function(i){
     return selectors[i].slice(1);
+  }
+
+  var pushElementWithId = function(){
+    if (elementsWithTag){
+      for (var i=0; i < elementsWithTag.length; i++){
+        addElementsWithIdentical(elementWithId, elementsWithTag[i]);
+      }
+    } else {
+      elements.push(elementWithId);
+    }
+  }
+
+  var pushElementsWithClass = function(){
+    if (elementsWithClass.length > 0){
+      elementsWithTag ? addSameElements(elementsWithTag, elementsWithClass) : addAllElementsFrom(elementsWithClass)
+    } else {
+      addAllElementsFrom(elementsWithTag)
+    }
   }
 
   for(var i=0; i < selectors.length; i++){
@@ -52,19 +74,9 @@ var $ = function (selector) {
   }
 
   if (elementWithId){
-    if (elementsWithTag){
-      for (var i=0; i < elementsWithTag.length; i++){
-        addElementsWithIdentical(elementWithId, elementsWithTag[i]);
-      }
-    } else {
-      elements.push(elementWithId);
-    }
+    pushElementWithId();
   } else {
-    if (elementsWithClass.length > 0){
-      elementsWithTag ? addSameElements(elementsWithTag, elementsWithClass) : addAllElementsFrom(elementsWithClass)
-    } else {
-      addAllElementsFrom(elementsWithTag)
-    }
+    pushElementsWithClass();
   }
 
   return elements;
